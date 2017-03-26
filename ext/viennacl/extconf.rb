e9627afa1500c37b2f5ff072e6294c60ca965a11
Takashi Tamura <tamuratak@users.noreply.github.com>
2017-03-26 23:05:41 +0900

    use mkmf-cu

--- ext/viennacl/extconf.rb
+++ ext/viennacl/extconf.rb
@@ -1,4 +1,4 @@
-require 'mkmf'
+require "mkmf"
 have_library("c++") or have_library("stdc++")
 
 gems = Gem::Specification.find_all_by_name("numo-narray")
@@ -9,17 +9,20 @@ find_header("numo/narray.h", File.join(gems[0].gem_dir, "lib/numo/") )
 
 have_func("rb_gc_adjust_memory_usage")
 
-device_flag = ""
-if arg_config("--enable-cuda") and defined?(MKMF_CU)
-  device_flag = " -DVIENNACL_WITH_CUDA -x cu "
+backend_flag = ""
+
+if arg_config("--enable-cuda")
+  require "mkmf-cu"
+  backend_flag += " -DVIENNACL_WITH_CUDA -x cu "
 elsif arg_config("--enable-opencl") and (have_library("OpenCL") or have_framework("OpenCL"))
-  device_flag = " -DVIENNACL_WITH_OPENCL "
+  backend_flag += " -DVIENNACL_WITH_OPENCL "
 end
+
 if arg_config("--enable-openmp")
-  device_flag += " -fopenmp -DVIENNACL_WITH_OPENMP "
+  backend_flag += " -fopenmp -DVIENNACL_WITH_OPENMP "
 end
 
 viennacl_path = File.join(File.dirname(File.expand_path(__FILE__)), "viennacl/")
 
-$CXXFLAGS = ($CXXFLAGS || "") + " -O2 -I #{viennacl_path} #{device_flag} "
+$CXXFLAGS = ($CXXFLAGS || "") + " -std=c++11 -I #{viennacl_path} #{backend_flag} "
 create_makefile('viennacl/ViennaCL')
