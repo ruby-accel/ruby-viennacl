d986e2890d874e4214ed746ddd61a13bec45a1bc
Takashi Tamura <tamuratak@users.noreply.github.com>
2017-03-28 13:54:57 +0900

    add a benchmark for numo/linalg

--- tool/bechmark.rb
+++ tool/bechmark.rb
@@ -1,4 +1,5 @@
 require 'numo/narray'
+require 'numo/linalg'
 require 'viennacl'
 require 'benchmark'
 
@@ -10,18 +11,26 @@ Benchmark.bmbm do |x|
   [["float", SFloat, SFloatMatrix], ["double", DFloat, DFloatMatrix]].each{|tag, na, cl|
     a = na.new(size, size).rand(1,10)
     b = na.new(size, size).rand(1,10)
-    x.report("numo/narray #dot #{tag}") do
-      c = a.dot b
+#     x.report("numo/narray #dot    #{tag}") do
+#       c = a.dot b
+#     end
+
+    x.report("numo/linalg #matmul #{tag}") do
+      50.times{
+        c = Linalg.matmul(a,b)
+      }
     end
 
     a1 = cl.from_narray(a)
     b1 = cl.from_narray(b)
-    x.report("viennacl    #dot #{tag}") do
-      c = a1.dot b1
+    x.report("viennacl    #dot    #{tag}") do
+      50.times {
+        c = a1.dot b1
+      }
       ViennaCL.finish()
     end
 
-    x.report("viennacl    copy #{tag}") do
+    x.report("viennacl    copy    #{tag}") do
       a1 = cl.from_narray(a)
       b1 = cl.from_narray(b)
     end
